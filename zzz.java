a6b4cd374c09f862a10daefad3b38813f620e131
gulman70 <gulfamchanna70@gmail.com>
2024-03-12 04:27:25 -0700

    error code

--- zzz.java
+++ zzz.java
@@ -1,3 +1,5 @@
+import javax.sql.rowset.spi.SyncResolver;
+
 class LOL {
 	public static void main(String args[]) {
 
@@ -7,6 +9,23 @@ class LOL {
 		System.out.println("working");
 		System.out.println("working");
 
+	public int add(int a, int b) {
+		return a + b;
 	}
 
-}
+	public static void main(String[] args) {  
+			Scanner scanner = new Scanner(System.in);  
+			  
+			System.out.print("Enter a number: ");  
+			int num1 = scanner.nextInt();  
+			  
+			System.out.print("Enter another number: ");  
+			int num2 = scanner.nextInt();  
+			int sum = add(num1, num2);  
+			System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+}System.out.println("after functions");}
