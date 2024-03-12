5bf3d30ca1a38e1f7e1963a9a5cd6e7e37e50d73
gulman70 <gulfamchanna70@gmail.com>
2024-03-12 04:22:49 -0700

    commit from another repo

--- zzz.java
+++ zzz.java
@@ -1,11 +1,8 @@
 class LOL {
 	public static void main(String args[]) {
-		System.out.println("working");
-		System.out.println("working");
-		System.out.println("working");
-		System.out.println("working");
 
-        System.out.println("working");
+		System.out.println("im champ.....");
+		System.out.println("working");
 		System.out.println("working");
 		System.out.println("working");
 		System.out.println("working");
